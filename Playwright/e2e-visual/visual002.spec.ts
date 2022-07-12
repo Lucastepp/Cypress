44da15ee5a5d76c86288f2014c8cc553711ccd11
Lucastepp <lucastep.uk@gmail.com>
2022-07-12 21:28:58 +0100

    0051

---
+++ Playwright/e2e-visual/visual002.spec.ts
@@ -0,0 +1,230 @@
+import test, { expect, Locator, Page } from "@playwright/test";
+
+const delay = (ms) =>
+new Promise((resolve) => setTimeout(resolve, ms));
+
+test.describe("Partner Dashboard Visual Regression", () => {
+  const partners = [
+    // "justeat",
+    // "shopify",
+    // "paymentsense",
+    // "rms",
+    // "dojo",
+    // "google",
+    // "mastercard",
+    // "takepayments",
+    // "ebayuk",
+    // "kinex",
+    // "payu/pl",
+    // "swoop",
+    // "boloo/nl",
+    // "foodhub",
+    // "eposnowcapital",
+    // "fundingcircle",
+    // "hdp", //handepay
+    // "paypoint",
+    // "tide",
+    // "wedoaccounting",
+     "brainpoint-be/be-fr",
+  ];
+
+  partners.forEach((partner) => {
+    test(
+      " 01 - Login In on Partner  = [ " + partner + " ]",
+      async ({ page }) => {
+        await page.goto(
+          "https://staging.youlend.com/apply/" + partner + "/signup"
+        );
+
+        
+        await delay(2000);
+
+        expect(await page.screenshot()).toMatchSnapshot(
+          `YL-${partner}Signup-Landing-page.png`
+        );
+      }
+    );
+
+    test(
+      " 02 - Checking page of Partner  = [ " + partner + " ]",
+      async ({ page }) => {
+        await page.goto(
+          "https://staging.youlend.com/apply/" + partner + "/signup"
+        );
+        expect(page.url()).toContain(`${partner}/signup`);
+      }
+    );
+
+    test(
+      " 03 - Printing Login button and Login in on  = [ " + partner + " ]",
+      async ({ page }) => {
+        await page.goto(
+          "https://staging.youlend.com/apply/" + partner + "/signup"
+        );
+
+        if (partner === "brainpoint-be/be-fr") {
+          const loginButton = page.locator(
+            "body > app-root > yl-header > mat-toolbar > div > div.user-controls > div.user-account-control"
+          );
+
+          await delay(500);
+          
+          expect(await loginButton.screenshot()).toMatchSnapshot(
+            `YL-${partner}Login-Button.png`
+          );
+
+          await delay(1500);
+
+          await page.click("text=Connexion");
+          expect(page.url()).toContain("https://youlend-stag.eu.auth0.com/");
+
+          await delay(2000);
+
+          expect(await page.screenshot()).toMatchSnapshot(
+            `YL-${partner}Auth0-page.png`
+          );
+        } else {
+          const loginButton = page.locator("text=Loginperson >> span");
+          expect(await loginButton.screenshot()).toMatchSnapshot(
+            `YL-${partner}Login-Button.png`
+          );
+
+          await delay(1500);
+
+          await page.click("text=Login");
+          expect(page.url()).toContain("https://youlend-stag.eu.auth0.com/");
+
+          await delay(2000);
+
+          expect(await page.screenshot()).toMatchSnapshot(
+            `YL-${partner}Auth0-page.png`
+          );
+        }
+
+        switch (partner) {
+          case "justeat":
+            await page.keyboard.type("lucas.pinto+0276@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "shopify":
+            await page.keyboard.type("lucas.pinto+0278@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "paymentsense":
+            await page.keyboard.type("lucas.pinto+0279@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "rms":
+            await page.keyboard.type("lucas.pinto+0280@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "dojo":
+            await page.keyboard.type("lucas.pinto+0281@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "google":
+            await page.keyboard.type("lucas.pinto+0282@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "mastercard":
+            await page.keyboard.type("lucas.pinto+0283@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "takepayments":
+            await page.keyboard.type("lucas.pinto+0284@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "ebayuk":
+            await page.keyboard.type("lucas.pinto+0285@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "kinex":
+            await page.keyboard.type("lucas.pinto+0286@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "payu/pl":
+            await page.keyboard.type("lucas.pinto+0287@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "swoop":
+            await page.keyboard.type("lucas.pinto+0288@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "boloo/nl":
+            await page.keyboard.type("lucas.pinto+0291@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "foodhub":
+            await page.keyboard.type("lucas.pinto+0292@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "eposnowcapital":
+            await page.keyboard.type("lucas.pinto+0293@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "fundingcircle":
+            await page.keyboard.type("lucas.pinto+0294@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "hdp":
+            await page.keyboard.type("lucas.pinto+0295@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "paypoint":
+            await page.keyboard.type("lucas.pinto+0296@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "tide":
+            await page.keyboard.type("lucas.pinto+0297@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "wedoaccounting":
+            await page.keyboard.type("lucas.pinto+0298@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+          case "brainpoint-be/be-fr":
+            await page.keyboard.type("lucas.pinto+0299@youlend.com");
+            await page.keyboard.down("Tab");
+            await page.keyboard.type("Password1!!");
+            break;
+
+          default:
+            throw Error("Partner Not Found!!!!");
+        }
+
+
+        await delay(500);
+
+        await page.click(
+          "#auth0-lock-container-1 > div > div.auth0-lock-center > form > div > div > div > button > span"
+        );
+
+        await delay(3000);
+
+        expect(await page.screenshot()).toMatchSnapshot(
+          `YL-${partner}-page.png`
+        );
+      }
+    );
+  });
+});
