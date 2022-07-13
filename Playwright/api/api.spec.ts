372ebf9ce71c70d28a8461630cab1c29cd55a9a1
Lucastepp <lucastep.uk@gmail.com>
2022-07-13 16:29:32 +0100

    0052

--- Playwright/api/api.spec.ts
+++ Playwright/api/api.spec.ts
@@ -6,10 +6,36 @@ test.describe('API Testing', () => {
     test('Simple API Test - Assert Response Status', async ({ request }) => {
         const response = await request.get(`${baseUrl}/users/2`)
         expect(response.status()).toBe(200)
+
+        const responseBody = JSON.parse(await response.text())
+        //console.log(responseBody)
     })
 
     test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
         const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
         expect(response.status()).toBe(404)
+    })
+
+    test('GET Request - Get User Detail', async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/1`)
+        const responseBody = JSON.parse(await response.text())
+
+        expect(response.status()).toBe(200)
+        expect(responseBody.data.id).toBe(1)
+        expect(responseBody.data.first_name).toBe('George')
+        expect(responseBody.data.last_name).toBe('Bluth')
+        expect(responseBody.data.email).toBeTruthy()
+        //console.log(responseBody)
+    })
+
+    test('POST Request - Create new user', async ({ request }) => {
+        const response = await request.post(`${baseUrl}/user`, {
+            data: {
+                id: 1000
+            }
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
     })
 })
