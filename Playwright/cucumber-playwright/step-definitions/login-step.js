7f0dc45902d4d9fe0cd2bba5dc9d561d6ec33051
Lucastepp <lucastep.uk@gmail.com>
2022-07-15 19:44:35 +0100

    0055

--- Playwright/cucumber-playwright/step-definitions/login-step.js
+++ Playwright/cucumber-playwright/step-definitions/login-step.js
@@ -1,27 +1,18 @@
+
 const { Given, When, Then, defineStep } = require('@cucumber/cucumber')
-const { LoginPage } = require('../page-objects/login-page')
 
-const loginPage = new LoginPage()
 
-defineStep('I visit a login page', async function () {
-  await loginPage.navigateToLoginScreen()
+Given('I visit login page', async function() {
+     await page.goto('https://www.saucedemo.com/')
 })
 
-defineStep('I fill the login form with valid credentials', async function () {
-  await loginPage.submitLoginForm()
+When('I fill the login form with valid credentials', async function()  {
+    await page.fill('#user-name', 'standard_user')
+    await page.fill('#password', 'secret_sauce')
+    await page.click('#login-button')
 })
 
-defineStep('I should see the home page', async function () {
-  await loginPage.assertUserIsLoggedIn()
+Then('I should see the home page', async function()  {
+    await page.waitForSelector('.inventory_list')
+   
 })
-
-defineStep('I wait for 3 seconds', async function () {
-  await loginPage.pause()
-})
-
-defineStep(
-  /^I fill the login form with "([^"]*)" and "([^"]*)"$/,
-  async function (username, password) {
-    await loginPage.submitLoginWithParameters(username, password)
-  }
-)
