66747873e642cd6ea76b828367e239be8de4109f
Lucastepp <lucastep.uk@gmail.com>
2022-07-17 01:32:40 +0100

    0057

---
+++ Angular/Angular-Masterclass/first-part/src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,42 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+
+describe('CalculatorService', () => {
+
+    it('Should add two numbers', () => {
+
+        //const logger = new LoggerService() // Removed cause it is best practice to use a fakespyobjct 
+
+        const logger = jasmine.createSpyObj('LoggerService', ['log'])
+        
+        //spyOn(logger, 'log'); Removed because there is a spy used on line 11 by Jasmine
+
+        const calculator = new CalculatorService(logger);
+
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4, 'Unexpected sum result')
+
+        expect(logger.log).toHaveBeenCalledTimes(1);
+
+        //pending();
+    })
+
+    it('Should subtract two numbers', () => {
+
+        const logger = new LoggerService()
+
+        spyOn(logger, 'log');
+        
+        const calculator = new CalculatorService(logger);
+
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0, 'Unexpected subtracted result')
+
+        expect(logger.log).toHaveBeenCalledTimes(1);
+        
+        //pending();
+    })
+});
